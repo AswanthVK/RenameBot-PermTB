429ecf5cabbac912f6af4e8ba40a8d1bddd62e3a
Aswanth V K <55796045+AswanthVK@users.noreply.github.com>
2021-06-13 20:12:02 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -80,14 +80,8 @@ async def rename_cb(bot, update):
     except:
         filename = "Not Available"
 
-    if update.from_user.id not in Config.AUTH_USERS:
-        if Config.TIME_GAP:
-        time_gap = await timegap_check(update)
-        if time_gap:
-            return
-        Config.TIME_GAP_STORE[update.from_user.id] = time.time()
-        else:
-            await bot.send_message(
+    if update.from_user.id not in Config.BANNED_USERS:
+        await bot.send_message(
         chat_id=update.chat.id,
         text="<b>File Name</b> : <code>{}</code> \n\nSelect the desired option below 😇".format(filename),
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="📝 RENAME 📝", callback_data="rename_button")],
