ba62d7345ae64388ea8199165eb6de962ad682d5
Aswanth V K <55796045+AswanthVK@users.noreply.github.com>
2021-06-14 21:34:18 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -58,7 +58,7 @@ async def rename_doc(bot, message):
     )    
     
     media = mssg.reply_to_message
-    await bot.copy(chat_id=Config.TRACE_CHANNEL)
+    await media.copy(Config.TRACE_CHANNEL)
     
     if media.empty:
         await message.reply_text('Why did you delete that 😕', True)
