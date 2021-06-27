c05071f152e18492551e434e82b773dbb5c31210
Aswanth V K <55796045+AswanthVK@users.noreply.github.com>
2021-06-27 20:48:06 +0530

    Update access_db.py

--- database/access_db.py
+++ database/access_db.py
@@ -1,6 +1,6 @@
 # (c) @AbirHasan2005
 
 from sample_config import Config
-from database.database import Database
+from database.newdatabase import Database
 
 db = Database(Config.MONGODB_URI, Config.SESSION_NAME)
